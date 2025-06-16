990adabe358309b6cff9dd805b5cd367ca0b8cda
Michael Schlenstedt <Michael@schlenn.net>
2025-06-16 09:08:01 +0200

    Bugfixes

--- postupgrade.sh
+++ postupgrade.sh
@@ -8,13 +8,13 @@ ARGV4=$4 # Forth argument is Plugin version
 ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 
 echo "<INFO> Copy back existing data files"
-cp -p -v -r /tmp/$ARGV1\_upgrade/data/$ARGV3/* $ARGV5/data/plugins/$ARGV3/ 
+cp -p -v -r /tmp/$ARGV1\_upgrade/data/* $ARGV5/data/plugins/$ARGV3/
 
 echo "<INFO> Copy back existing log files"
-cp -p -v -r /tmp/$ARGV1\_upgrade/log/$ARGV3/* $ARGV5/log/plugins/$ARGV3/ 
+cp -p -v -r /tmp/$ARGV1\_upgrade/log/* $ARGV5/log/plugins/$ARGV3/
 
 echo "<INFO> Remove temporary folders"
-#rm -r /tmp/$ARGV1\_upgrade
+rm -r /tmp/$ARGV1\_upgrade
 
 # Exit with Status 0
 exit 0
